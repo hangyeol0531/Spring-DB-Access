9b0698d4bc99784cd7ed5077411f2f32161650cb
hangyeol0531 <jhan0531@naver.com>
2023-11-21 00:01:04 +0900

    checked, unchecked exception

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedTest.java
@@ -0,0 +1,54 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class UnCheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(service::callThrow)
+            .isInstanceOf(MyUnCheckedException.class);
+    }
+
+    /**
+     * RuntimeException 상속받은 예외는 언체크 예외
+     */
+    static class MyUnCheckedException extends RuntimeException {
+        public MyUnCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUnCheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUnCheckedException("ex");
+        }
+    }
+}
