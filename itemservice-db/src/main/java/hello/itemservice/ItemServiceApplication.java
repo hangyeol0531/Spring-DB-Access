ff21b24c819bf9df1ec65634d108b09359ccd06c
hangyeol0531 <jhan0531@naver.com>
2023-12-20 00:07:15 +0900

    spring data access 2 : embedded database

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,9 +1,8 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV1Config;
-import hello.itemservice.config.JdbcTemplateV2Config;
 import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -28,4 +28,15 @@ public class ItemServiceApplication {
         return new TestDataInit(itemRepository);
     }
 
+//    @Bean
+//    @Profile("test")
+//    public DataSource dataSource() {
+//        log.info("memory database init");
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
 }
