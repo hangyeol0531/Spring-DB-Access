09e8065fccb0b36b2a292951f1697ce09e3c9deb
hangyeol0531 <jhan0531@naver.com>
2023-12-20 23:44:59 +0900

    spring data access 2 : etc

--- itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -17,9 +17,9 @@ public interface ItemMapper {
     // parameter가 2개인 경우 @Param으로 구분해야한다.
     void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto itemUpdateDto);
 
-    //BeanPropertyRowMapper 처럼 Select 결과를 객체로 바로 변환해준다.
+    // BeanPropertyRowMapper 처럼 Select 결과를 객체로 바로 변환해준다.
+    // @Select("select id, item_name, price, quantity from item where id=#{id}")
     Optional<Item> findById(Long id);
-
 
     List<Item> findAll(ItemSearchCond itemSearchCond);
 
