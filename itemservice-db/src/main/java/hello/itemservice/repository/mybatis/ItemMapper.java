4d3f7f597e1cff6cd43920946e5e8dc2a5872d1f
hangyeol0531 <jhan0531@naver.com>
2023-12-20 22:44:38 +0900

    spring data access 2 : mybatis init, basic

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,26 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    // parameter가 2개인 경우 @Param으로 구분해야한다.
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto itemUpdateDto);
+
+    //BeanPropertyRowMapper 처럼 Select 결과를 객체로 바로 변환해준다.
+    Optional<Item> findById(Long id);
+
+
+    List<Item> findAll(ItemSearchCond itemSearchCond);
+
+}
